16de5adeea5b6d8f81f3248a19620dce486cd8b3
priyanshujains <jainpriyanshu0129@gmail.com>
2025-05-25 14:30:36 +0530

    Inistialising Project

---
+++ src/test/java/com/example/IncognitoCampus/IncognitoCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IncognitoCampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncognitoCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
